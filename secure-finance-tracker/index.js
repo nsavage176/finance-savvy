dd3ea1e4bd0eabeb5260f05d6e2de9b42667bab1
Nicole Savage <akp5ve@virginia.edu>
2024-12-13 13:00:57 -0500

    Initialized server and connected to MongoDB

---
+++ secure-finance-tracker/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const helmet = require('helmet');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
+const DB_URI = process.env.MONGO_URI;
+mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+app.listen(5000, () => {
+    console.log('Server is running on port 5000');
+});
